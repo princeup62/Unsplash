aa2a51e3a8b881f1bff195e2bac7e5ff3c8bd332
princeup62 <princesh208@gmail.com>
2021-01-26 18:10:18 +0530

    API Base are added

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,20 +1,12 @@
 
+import axios from 'axios';
 import React,{useEffect, useState,useRef} from 'react'
 import Image from './Image';
 
  function Images() {
 
-  const [ImgSrc,setImageSrc]=useState([
-   "https://images.unsplash.com/photo-1536662788222-6927ce05daea?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-  "https://images.unsplash.com/photo-1552993906-7131796c7310?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-  "https://images.unsplash.com/photo-1610219789009-7bfbca2d8af7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
-  "https://images.unsplash.com/photo-1552327803-2c89bc3e1483?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-  "https://images.unsplash.com/photo-1494245829061-728222e74963?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=307&q=80",
-  "https://images.unsplash.com/photo-1560272197-6eed4801462f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-  "https://images.unsplash.com/photo-1565286364541-5f0326e869ad?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-  "https://images.unsplash.com/photo-1600250410329-f8ea0894b24f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-  "https://images.unsplash.com/photo-1516373829531-29d21ac7f9d6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=348&q=80"
-  ]);
+  const [ImgSrc,setImageSrc]=useState([]);
+
   const [inputData,setInputData]=useState("");
 
   useEffect (()=>{
@@ -24,7 +16,8 @@ import Image from './Image';
 console.log(inputRef);
 
 inputRef.current.focus();
-
+axios.get('https://api.unsplash.com/photos/?client_id=iGcQJSuPPSJt4UBbnoPi5lE8op8FM88oUeEn8mdxGkM')
+.then((res)=>setImageSrc(res.data))
   },[]);
 
   const inputRef = useRef(null);
@@ -70,7 +63,7 @@ inputRef.current.focus();
 
 
       <section className="Image-map-wrapper">
-      {ImgSrc.map((data,index)=> <Image imageSource={data} 
+      {ImgSrc.map((data,index)=> <Image imageSource={data.urls.regular} 
       handleDelete={handleDelete} key={index} id={index} index={index} />)}
       </section>
       
